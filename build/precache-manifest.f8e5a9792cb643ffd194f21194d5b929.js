096d4c2f0cd9ae5e577bf35618543e8e0c7b335d
seaerchin <jer005@e.ntu.edu.sg>
2020-06-15 13:36:33 +0800

    built

---
+++ build/precache-manifest.f8e5a9792cb643ffd194f21194d5b929.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a98adf6243cc1de5c4531f6b6c27dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "683e63b000f44696ed7c",
+    "url": "/static/js/2.b12358a8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b12358a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f722e20d1f944c514d2",
+    "url": "/static/js/main.b00c0f10.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "a6312b4ab1470afcabaceb69261a4bbb",
+    "url": "/static/media/About.a6312b4a.svg"
+  },
+  {
+    "revision": "ef86398f871de6956eee2e64566f3a07",
+    "url": "/static/media/Small.ef86398f.svg"
+  },
+  {
+    "revision": "0616c2867657e679388be0727dd763ef",
+    "url": "/static/media/Tech.0616c286.svg"
+  },
+  {
+    "revision": "1ea9374c20d9e11603243525d86b995a",
+    "url": "/static/media/ninja.1ea9374c.png"
+  },
+  {
+    "revision": "a349ed648e5464d0672f67e7ffc60ab7",
+    "url": "/static/media/ntu.a349ed64.png"
+  },
+  {
+    "revision": "2831db8775f232d01be685a53c8834d1",
+    "url": "/static/media/renal_logo.2831db87.png"
+  }
+]);
